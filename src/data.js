1c39aefcd2f7ff704f9cd47512c179d46fc540c6
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-10-18 00:46:56 +1300

    first commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const projects = [
+	{
+		title: 'Advice generator',
+		url: 'https://advice-generetor-app-c0bf99.netlify.app/',
+		image: './assets/Advice generator.png',
+	},
+	{
+		title: 'Art Gallery',
+		url: 'https://art-gallery-website-seven.vercel.app/',
+		image: './assets/Art Gallery.png',
+	},
+	{
+		title: 'Equalizer App',
+		url: 'https://equalizer-landing-page-delta.vercel.app/',
+		image: './assets/Equilizer App.png',
+	},
+	{
+		title: 'FAQ Component',
+		url: 'https://faq-accordion-card-react-6jtcwdpx8-hcmwebs.vercel.app/',
+		image: './assets/FAQ component.png',
+	},
+	{
+		title: 'Fylo',
+		url: 'https://fylo-dark-theme-hazel.vercel.app/',
+		image: './assets/Fylo.png',
+	},
+	{
+		title: 'Officelite',
+		url: 'https://officelite-coming-soon-site-hcmwebs.vercel.app/',
+		image: './assets/Officelite.png',
+	},
+	{
+		title: 'Skilled E-learning',
+		url: 'https://skilled-e-learning-landing-7ovavr9od-hcmwebs.vercel.app/',
+		image: './assets/Skilled e-learning.png',
+	},
+	{
+		title: 'Sunnyside Agency',
+		url: 'https://sunnyside-agency-1u3o8zs4f-hcmwebs.vercel.app/',
+		image: './assets/Sunnyside agency.png',
+	},
+	{
+		title: 'Typemaster Keyboard',
+		url: 'https://typemaster-prelaunch-page-beige.vercel.app/',
+		image: './assets/Typemaster Keyboard.png',
+	},
+];
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import GlobalStyles from './styles/GlobalStyles.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<StrictMode>
+		<GlobalStyles />
+		<App />
+	</StrictMode>
+);
